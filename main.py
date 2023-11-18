50b12b3f7b378cdc386920a2e94c3251355f27b3
NEK1Tocik <gold81963@gmail.com>
2023-11-18 10:16:37 +0300

    Telegram Bot

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
+import time
+from aiogram.types import InputFile
+from PIL import Image, ImageFont, ImageDraw
 
 # Установка уровня логирования
 logging.basicConfig(level=logging.INFO)
@@ -37,7 +40,32 @@ async def show_menu(message: types.Message):
     item1 = types.KeyboardButton('🛒 Показать товары')
     item2 = types.KeyboardButton('ℹ️ Информация')
     markup.add(item1, item2)
-    await message.answer("Привет! Я бот. Чем могу помочь? Кликай по кнопком в зависимости от твоих желаний).", reply_markup=markup)
+    await message.answer("Привет! Я бот. Чем могу помочь? Кликай по кнопком в зависимости от твоих желаний) Также при команде /photo появится фото из интернета. В общем всю информацию ты можешь узнать нажав кнопку информация.", reply_markup=markup)
+
+@dp.message_handler(content_types=['photo'])
+async def handle_docs_photo(message: types.Message):
+    await message.photo[-1].download('r1.png')
+    time.sleep(1)
+    sample = Image.open('r1.png')
+    font = ImageFont.truetype('Minecraft.otf', size=154, encoding='ASCII')
+    draw = ImageDraw.Draw(sample)
+    draw.text((250, 500), font=font, text="Hello world", align="center", fill='blue')
+    sample.save('r3.png')
+    sample1 = Image.open('r1.png')
+    font1 = ImageFont.truetype('tahoma.ttf', size=154, encoding='ASCII')
+    draw1 = ImageDraw.Draw(sample1)
+    draw1.text((250, 500), font=font1, text="Hello world", align="center", fill='blue')
+    sample1.save('r2.png')
+    await message.answer_photo(InputFile('r3.png'))
+    await message.answer_photo(InputFile('r2.png'))
+
+# Обработчик команды /photo для отправки изображения
+@dp.message_handler(commands=['photo'], state="*")
+async def send_photo(message: types.Message):
+    # Замените 'photo_url' на URL-адрес изображения, которое вы хотите отправить
+    photo_url = 'https://kartinkof.club/uploads/posts/2023-05/1683441129_kartinkof-club-p-programmirovanie-kartinki-8.jpg'
+    caption = 'Вот ваше изображение из интернета!'
+    await bot.send_photo(message.chat.id, photo=photo_url, caption=caption)
 
 # Обработчик кнопки "Показать товары" в меню
 @dp.message_handler(text='🛒 Показать товары', state="*")
@@ -61,7 +89,7 @@ async def process_show_product(callback_query: types.CallbackQuery):
 # Обработчик кнопки "ℹ️ Информация" в меню
 @dp.message_handler(text='ℹ️ Информация', state="*")
 async def process_information(message: types.Message):
-    await message.answer("Это бот, который содержит товары и имитацию их покупок! Жми кнопку Показать товары, чтобы посмотреть все доступные товары.")
+    await message.answer("Я бот, который содержит товары и имитацию их покупок! Жми кнопку Показать товары, чтобы посмотреть все доступные товары. При написании команды /photo появится фото из интернета. Если хочешь чтобы я написал на твоей картинке hello world присылай мне картинку.")
 
 # Запуск бота
 if __name__ == '__main__':
