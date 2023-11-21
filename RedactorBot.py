757f6ce97265b27308592e5e95cb30457abfd8be
NEK1Tocik <gold81963@gmail.com>
2023-11-21 20:29:48 +0300

    Telegram Bot

--- RedactorBot.py
+++ RedactorBot.py
@@ -4,7 +4,7 @@ import asyncio
 from PIL import Image
 
 # Устанавливаем токен вашего бота
-API_TOKEN = 'YOUR_BOT_TOKEN'
+API_TOKEN = 'YOUR_BOT_TOKEN' # Сюда токен вашего бота
 
 # Инициализируем бот и диспетчер
 bot = Bot(token=API_TOKEN)
